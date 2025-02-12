166b35da184aaf8a01ddd7b0e021ba89707ff5ca
chethazz <chethaskp10@gmail.com>
2025-02-12 13:25:09 +0530

    [slug]: not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return (
+        <main className="px-5 py-10 mx-auto space-y-5 text-center max-w-7xl">
+            <h1 className="text-3xl font-bold">
+                Not found
+            </h1>
+            <p>
+                This page does not exist!
+            </p>
+        </main>
+    );
+}
