c4d84adeaeb2dfdb82b161926aef67d912431485
chethazz <chethaskp10@gmail.com>
2025-02-12 10:10:22 +0530

    components: ui: Badge

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,7 @@
 import { products } from "@wix/stores";
 import Link from "next/link";
 import WixImage from "./WixImage";
+import Badge from "./ui/badge";
 
 interface ProductProps {
     product: products.Product;
@@ -17,7 +18,7 @@ export default function Product({
             href={`/products/${product.slug}`}
             className="h-full border"
         >
-            <div className="overflow-hidden">
+            <div className="relative overflow-hidden">
                 <WixImage
                     mediaIdentifier={mainImage?.url}
                     alt={mainImage?.altText || ""}
@@ -25,6 +26,13 @@ export default function Product({
                     height={700}
                     className="transition-transform duration-300 hover:scale-110"
                 />
+                <div className="absolute flex flex-wrap items-center gap-2 bottom-3 right-3">
+                    {product.ribbon && (
+                        <Badge>
+                            {product.ribbon}
+                        </Badge>
+                    )}
+                </div>
             </div>
             <div className="p-3 space-y-3">
                 <h3 className="text-lg font-bold">{product.name}</h3>
