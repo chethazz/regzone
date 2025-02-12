654ce1f5b2cbf605583c80973d48da52628a91d8
chethazz <chethaskp10@gmail.com>
2025-02-12 11:26:50 +0530

    app: terms: page

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,69 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Terms and Conditions",
+    description: "Terms and conditions for using our services",
+};
+
+export default function Page() {
+    return (
+        <main className="max-w-4xl px-5 py-10 mx-auto space-y-10 leading-7">
+            <div className="mx-auto">
+                <h2 className="py-5 text-3xl">CUSTOMER CARE</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+                    ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                    sunt explicabo.
+                </p>
+                <h2 className="py-5 mt-10 text-3xl">PRIVACY & SAFETY</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+                    ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                    sunt explicabo.
+                </p>
+                <h2 className="py-5 mt-10 text-3xl">WHOLESALE INQUIRIES</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+                    ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                    sunt explicabo.
+                </p>
+                <h2 className="py-5 mt-10 text-3xl">PAYMENT METHODS</h2>
+                <p>
+                    - Credit / Debit Cards
+                    <br /> - PAYPAL
+                    <br /> - Cash on delivery
+                </p>
+            </div>
+        </main>
+    );
+}
