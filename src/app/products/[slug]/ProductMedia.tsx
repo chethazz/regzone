424925a33d7caa0db9109bb91cc113fa89095edd
chethazz <chethaskp10@gmail.com>
2025-02-14 08:28:14 +0530

    [slug]: ProductMedia: React medium zoom

--- src/app/products/[slug]/ProductMedia.tsx
+++ src/app/products/[slug]/ProductMedia.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import { products } from "@wix/stores";
 import { PlayIcon } from "lucide-react";
 import { useState } from "react";
+import Zoom from "react-medium-image-zoom";
 
 interface ProductMediaProps {
     media: products.MediaItem[] | undefined;
@@ -23,12 +24,14 @@ export default function ProductMedia({
         <div className="space-y-5 basis-2/5 md:sticky md:top-0 h-fit">
             <div className="aspect-square bg-secondary">
                 {selectedImage?.url ? (
-                    <WixImage
-                        mediaIdentifier={selectedImage.url}
-                        alt={selectedImage.altText}
-                        width={1000}
-                        height={1000}
-                    />
+                    <Zoom>
+                        <WixImage
+                            mediaIdentifier={selectedImage.url}
+                            alt={selectedImage.altText}
+                            width={1000}
+                            height={1000}
+                        />
+                    </Zoom>
                 ) : selectedVideo?.url ? (
                     <div className="flex items-center bg-black size-full">
                         <video
