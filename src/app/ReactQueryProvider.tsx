0d04b56096f23f1df22fb6af0afb2fe9d6f0c613
chethazz <chethaskp10@gmail.com>
2025-02-17 09:46:11 +0530

    app: ReactQueryProvider: Fix

--- src/app/ReactQueryProvider.tsx
+++ src/app/ReactQueryProvider.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React, { useState } from "react";
 
-export default async function ReactQueryProvider({
+export default function ReactQueryProvider({
     children
 }: { children: React.ReactNode; }) {
     const [client] = useState(new QueryClient());
